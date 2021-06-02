eaa538039bdc20dc0fdf32c65fb13f5ca5d184ff
Sergio Andres Munoz Castillo <sergio-tuza@live.com.mx>
2021-06-02 14:39:44 -0500

    COmmit de la leccion 15 (innecesaria)

---
+++ project/foro/app/Filters/ThreadFilters.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filters;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ThreadFilters
+{
+
+    /**
+     * @var Request
+     */
+    protected $request;
+    private $builder;
+        /**
+         * ThreadFilters constructor
+         */
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function apply($builder)
+    {
+        $this->builder = $builder;
+
+        if ($this->request->has('by')){
+            $this->by($this->request->by);
+        }
+        return $this->builder;
+    }
+
+    protected function by($username)
+    {
+        $user = User::where('name', $username)->firstOrFail();
+
+        return $this->builder->where('user_id', $user->id);
+    }
+}
