5b76da5b674bd4d1af251dd18d936ba742cd7787
sergio <sergio-tuza@live.com.mx>
2021-06-03 00:03:04 -0500

    Lecciones 20 y 21

--- project/foro/resources/views/threads/reply.blade.php
+++ project/foro/resources/views/threads/reply.blade.php
@@ -1,6 +1,18 @@
 <div class="panel panel-default">
   <div class="panel-heading">
-    <a href="#">  {{ $reply->owner->name }}</a> said {{ $reply->created_at}}
+
+    <div class="level">
+      <h5 class="flex">
+        <a href="#">  {{ $reply->owner->name }}</a> said {{ $reply->created_at}}
+      </h5>
+
+        <form class="" action="/replies/{{ $reply->id }}/favorites" method="POST">
+          @csrf
+          <button type="submit" class="btn btn-default" {{ $reply->isFavorited()? 'disabled' : '' }}>
+            {{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+          </button>
+        </form>
+    </div>
   </div>
 
     <div class="panel-body">
